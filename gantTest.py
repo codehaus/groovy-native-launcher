adc852521e9cb3a9d708bcfe070da429277c69ce
russel <russel@a5544e8c-8a19-0410-ba12-f9af4593a198>
2008-05-29 18:53:13 +0000

    Prepare the ground for Gant as well as Groovy tests.
    git-svn-id: https://svn.codehaus.org/groovy/trunk/groovy/modules/native_launcher@12668 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ gantTest.py
@@ -0,0 +1,31 @@
+# -*- mode:python coding:utf-8 -*-
+
+#  Groovy -- A native launcher for Groovy
+#
+#  Copyright © 2008 Russel Winder
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+#  compliance with the License. You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software distributed under the License is
+#  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+#  implied. See the License for the specific language governing permissions and limitations under the
+#  License.
+
+#  The actual test cases are collected together in this class.
+
+class GantTestCase ( unittest.TestCase ) :
+    pass
+
+#  The entry point for SCons to use.
+
+def runTests ( path , architecture ) :
+    supportModule.executablePath = path
+    supportModule.platform = architecture
+    suite = unittest.makeSuite ( GroovyTestCase , 'test' )
+    return unittest.TextTestRunner ( ).run ( suite ).wasSuccessful ( )
+
+if __name__ == '__main__' :
+    print 'Run tests using command "scons test".'
