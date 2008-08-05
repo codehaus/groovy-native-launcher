b421d70dc282c8414210793a36a7e865d0485ae1
russel <russel@a5544e8c-8a19-0410-ba12-f9af4593a198>
2008-08-05 08:19:35 +0000

    Get rid of the error messages that can be got rid of when compiling on Mac OS X Leopard with Xcode 3.0.
    git-svn-id: https://svn.codehaus.org/groovy/trunk/groovy/modules/native_launcher@13140 a5544e8c-8a19-0410-ba12-f9af4593a198

--- source/jst_paramhandling.c
+++ source/jst_paramhandling.c
@@ -18,9 +18,20 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#if defined ( __APPLE__ )
+#  include <TargetConditionals.h>
+
+/*
+ *  The Mac OS X Leopard version of jni_md.h (Java SE 5 JDK) is broken in that it tests the value of
+ *  __lp64__ instead of the presence of _LP64 as happens in Sun's Java 6.0 JDK.  To prevent spurious
+ *  warnings define this with a false value.
+ */
+#define __LP64__ 0
+
+#endif
+
 #include "jvmstarter.h"
 #include "jst_dynmem.h"
-
 
 #if defined( _WIN32 )
 #  include <Windows.h>
