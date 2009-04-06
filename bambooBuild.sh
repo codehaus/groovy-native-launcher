94ca6536aff3ae39c53f1d0bb6d8d2d53d45fd83
russel <russel@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-04-06 10:57:28 +0000

    Aha, the trick is to use absolute paths to get to executables that are there but which Bamboo censors off the standard path.
    git-svn-id: https://svn.codehaus.org/groovy/trunk/groovy/modules/native_launcher@15879 a5544e8c-8a19-0410-ba12-f9af4593a198

--- bambooBuild.sh
+++ bambooBuild.sh
@@ -1,10 +1,14 @@
 #! /bin/sh
 
+#  Bamboo filddles somewhat with the list of execiutables on the standard path so sometimes have to force
+#  things by using absolute paths.  In particular wget, curl and unzip are available but not on the stnadrd
+#  path.
+ 
 groovyVersion=1.6.0
 groovyZipName=groovy-binary-$groovyVersion.zip
 
-curl  http://dist.codehaus.org/groovy/distributions/$groovyZipName > $groovyZipName
-unzip $groovyZipName
+/usr/bin/wget http://dist.codehaus.org/groovy/distributions/$groovyZipName
+/usr/bin/unzip $groovyZipName
 
 export GROOVY_HOME=groovy-$groovyVersion
 
