0fc67cdd619c62b6661154262d1928f2c2474dbc
russel <russel@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-04-06 11:43:47 +0000

    Get the Groovy install path correct for the experiment.
    git-svn-id: https://svn.codehaus.org/groovy/trunk/groovy/modules/native_launcher@15882 a5544e8c-8a19-0410-ba12-f9af4593a198

--- bambooBuild.sh
+++ bambooBuild.sh
@@ -6,6 +6,7 @@
  
 groovyVersion=1.6.0
 groovyZipName=groovy-binary-$groovyVersion.zip
+groovyInstallPath=groovy-$groovyVersion
 
 /usr/bin/wget http://dist.codehaus.org/groovy/distributions/$groovyZipName
 /usr/bin/unzip $groovyZipName
@@ -13,9 +14,9 @@ groovyZipName=groovy-binary-$groovyVersion.zip
 
 # Execute an experimental run of groovy to collect data about the server VM.
 
-__JLAUNCHER_DEBUG=true  groovy -server -e "println 'hello'" 
+__JLAUNCHER_DEBUG=true  $groovyInstallPath/bin/groovy -server -e "println 'hello'" 
 
 
-export GROOVY_HOME=groovy-$groovyVersion
+export GROOVY_HOME=$groovyInstallPath
 
 scons test
