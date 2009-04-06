8aae0f023f0c1238230e41edba537deb6356d1fa
russel <russel@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-04-06 09:06:41 +0000

    Codehaus doesn't have wget installed, try curl instead.
    git-svn-id: https://svn.codehaus.org/groovy/trunk/groovy/modules/native_launcher@15875 a5544e8c-8a19-0410-ba12-f9af4593a198

--- bambooBuild.sh
+++ bambooBuild.sh
@@ -3,7 +3,7 @@
 groovyVersion=1.6.0
 groovyZipName=groovy-binary-$groovyVersion.zip
 
-wget  http://dist.codehaus.org/groovy/distributions/$groovyZipName
+curl  http://dist.codehaus.org/groovy/distributions/$groovyZipName > $groovyZipName
 unzip $groovyZipName
 
 export GROOVY_HOME=groovy-$groovyVersion
