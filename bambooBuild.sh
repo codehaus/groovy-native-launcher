320d6a4ac09f51a01c07ce039bce90431b4e8d15
russel <russel@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-04-28 12:21:59 +0000

    Add stuff to the Bamboo build for the 64-bit plan.
    git-svn-id: https://svn.codehaus.org/groovy/trunk/groovy/modules/native_launcher@16191 a5544e8c-8a19-0410-ba12-f9af4593a198

--- bambooBuild.sh
+++ bambooBuild.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 
+#  By default build for 32-bit systems.  If the parameter is 64 then build for 64-bit systems.
+
+if [ $# -eq 1 ]
+then
+    if [ $1 -eq 64 ]
+    then
+        widthParameter="width=64"
+    fi
+fi
+
 #  Bamboo filddles somewhat with the list of execiutables on the standard path so sometimes have to force
 #  things by using absolute paths.  In particular wget, curl and unzip are available but not on the stnadrd
 #  path.
@@ -16,4 +26,5 @@ export GROOVY_HOME=$groovyInstallPath
 #  Remove any inherited values in JAVA_OPTS since they shouldn't be there.  Is the return code from scons
 #  the return code of the script?
 
-__JLAUNCHER_DEBUG=true JAVA_OPTS=  scons test
+#__JLAUNCHER_DEBUG=true JAVA_OPTS=  scons $widthParameter test
+JAVA_OPTS=  scons $widthParameter test
