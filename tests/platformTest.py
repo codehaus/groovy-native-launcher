03b9fd22f57d282f8b68105248467514f355a2d9
akaranta <akaranta@a5544e8c-8a19-0410-ba12-f9af4593a198>
2010-11-03 06:17:48 +0000

    added test that JAVA_HOME points to a jvm with same word size as the build (produced binaries will be ok but tests will fail. Now there is at least a clear message as to why they fail).
    git-svn-id: https://svn.codehaus.org/groovy/trunk/groovy/modules/native_launcher@21049 a5544e8c-8a19-0410-ba12-f9af4593a198

--- tests/platformTest.py
+++ tests/platformTest.py
@@ -5,6 +5,8 @@ import unittest
 import shutil
 import platform
 import ctypes
+import tempfile
+import subprocess
 
 import supportModule
 
@@ -18,10 +20,30 @@ class PlatformTestCase ( unittest.TestCase ) :
     #       many tests will fail with mysterious error messages
     #       by which it may not be clear to what the problem is
 
-    def testWidthsMatch( self ) :
+    def testBuildWidthMatchesPythonInterpreterWidth( self ) :
         self.assertEqual( width, supportModule.width, 'this python process and your build have different word size' )
-        # TODO: test that the jvm at JAVA_HOME has the same width
-        pass
+
+    def testJavaWidthMatchesBuildWidth( self ) :
+        javaSource = """
+public class WidthTeller {
+    public static void main( String[] args ) {
+        System.out.println( System.getProperty( "sun.arch.data.model" ) ) ;
+    }
+}
+"""
+        tmpdir = tempfile.gettempdir()
+        javaSourceFileName = os.path.join( tmpdir, "WidthTeller.java" ) 
+        with open( javaSourceFileName, "w" ) as javaSourceFile :
+            javaSourceFile.write( javaSource )
+        javaHome = os.environ[ "JAVA_HOME" ]
+        javac = os.path.join( javaHome, 'bin', 'javac' )
+        subprocess.check_call( [ javac, javaSourceFileName ] )
+        java = os.path.join( javaHome, 'bin', 'java' )
+        result = subprocess.check_output( [ java, '-cp', tmpdir, 'WidthTeller' ] )
+        result = result.strip()
+        message = "JAVA_HOME points to a Java installation with different word size ({0}bit) than the current build ({1}bit).\nThe binaries produced are not affected, but the tests will fail.".format( result, width )
+        self.assertEqual( str( supportModule.width ), result, message )
+        # TODO: delete java source and compiled bytecode
 
 
 
